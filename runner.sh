8c90af987841ea7115ba4ae62dba7d12fa6f6f0a
iminin <i.minin@ongrid.pro>
2021-12-14 19:34:54 +0300

    chore: init project

---
+++ runner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+start=$1
+end=$2
+way=$((end))-$((start))
+cpu_count=$(grep -c ^processor /proc/cpuinfo)
+cpu_count=$((cpu_count-2))
+step=$(($((way))/$((cpu_count))))
+echo "use CPU_COUNT=$cpu_count"
+counter=$start
+docker build --tag indexer .
+if (( $((way))>=200 ))
+then
+  for (( i=1; i <= $((cpu_count)); i++ ))
+    do
+      if (( $((counter-1+step))>=end ))
+      then
+        end_b=end
+      else
+        end_b=$((start+step))
+      fi
+      container_name="indexer_$((i))_start_$((start))_end_$((end_b))"
+      echo "run: "$container_name
+      docker run -d --name $container_name -e INDEXER_START_BLOCK=$((start)) -e INDEXER_END_BLOCK=$((end_b)) indexer
+      counter=$((counter+step+1))
+      start=$((start+step+1))
+    done
+fi
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python indexer.py $((INDEXER_START_BLOCK)) $((INDEXER_END_BLOCK))
