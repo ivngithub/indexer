24f0404bac37b0438a780c7b4cd8e44e5210be46
iminin <i.minin@ongrid.pro>
2021-12-14 19:43:17 +0300

    chore: create django project

---
+++ indexer_worker.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+
+start_block = sys.argv[1]
+end_block = sys.argv[2]
+
+
+print(start_block, end_block)
+time.sleep(30)
--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python indexer.py $((INDEXER_START_BLOCK)) $((INDEXER_END_BLOCK))
+python indexer_worker.py $((INDEXER_START_BLOCK)) $((INDEXER_END_BLOCK))
