be0133c16b7622896fa4007668d1e9789ab33ffd
iminin <i.minin@ongrid.pro>
2021-12-18 17:47:35 +0300

    chore: add Account Trade and Token models

--- indexer/admin.py
+++ indexer/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from indexer.models import Contract, Indexer, RangeBlock
+from indexer.models import Contract, Indexer, RangeBlock, Account, Trade, Token
 
 
 @admin.register(Contract)
@@ -15,6 +15,21 @@ class IndexerAdmin(admin.ModelAdmin):
     )
 
 
-@admin.register(RangeBlock )
+@admin.register(RangeBlock)
 class RangeBlockAdmin(admin.ModelAdmin):
     list_display = ('first_block', 'last_block', 'count_events', 'contract', 'indexer', 'created_at')
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('address', 'points')
+
+
+@admin.register(Trade)
+class TradeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'seller', 'buyer', 'tx', 'block_number', 'block_hash', 'range_block')
+
+
+@admin.register(Token)
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'address', 'start_block', 'end_block', 'coefficient')
--- indexer/models.py
+++ indexer/models.py
@@ -9,8 +9,17 @@ class Contract(models.Model):
     end_block = models.IntegerField()
 
 
+class Token(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    address = models.CharField(max_length=42, unique=True)
+    abi = models.JSONField()
+    start_block = models.IntegerField()
+    end_block = models.IntegerField()
+    coefficient = models.IntegerField(default=1)
+
+
 class Indexer(models.Model):
-    indexer_id = models.IntegerField(unique=True)
+    indexer_id = models.IntegerField()
     start_block = models.IntegerField()
     end_block = models.IntegerField()
     last_block = models.IntegerField()
@@ -22,18 +31,23 @@ class Indexer(models.Model):
 class RangeBlock(models.Model):
     first_block = models.IntegerField()
     last_block = models.IntegerField()
+    step = models.IntegerField()
     count_events = models.IntegerField()
     contract = models.ForeignKey(Contract, on_delete=models.CASCADE)
     indexer = models.ForeignKey(Indexer, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-#
-#
-# class Account(models.Model):
-#     address = models.CharField(max_length=42, unique=True)
-#
-#
-# class Trade(models.Model):
-#     seller = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='seller_of')
-#     buyer = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='buyer_of')
-#     tx = models.CharField(max_length=200, unique=True)
-#     block = models.ForeignKey(RangeBlock, on_delete=models.CASCADE)
+
+
+class Account(models.Model):
+    address = models.CharField(max_length=42, unique=True)
+    points = models.IntegerField(default=0)
+
+
+class Trade(models.Model):
+    seller = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='seller_of')
+    buyer = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='buyer_of')
+    tx = models.CharField(max_length=200, unique=True)
+    block_number = models.IntegerField()
+    block_hash = models.CharField(max_length=200)
+    range_block = models.ForeignKey(RangeBlock, on_delete=models.CASCADE)
+
